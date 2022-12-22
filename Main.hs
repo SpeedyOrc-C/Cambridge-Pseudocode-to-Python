2888509dfca47a7671d5eb5b469794d640dbad84
陈湛明 <1670732206@qq.com>
2022-12-22 23:59:51 +0000

    Add compile script and CLI

---
+++ Main.hs
@@ -0,0 +1,27 @@
+module Main (main) where
+
+import MyParser (run)
+import CambridgePseudocodeToPython ( cpFlowP, dump, initialTranslatorState )
+
+import Data.Maybe (fromJust, isJust)
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+    args <- getArgs
+    if null args then
+        putStrLn "No input file specified."
+    else do
+        let inputPath = head args
+            outputPath = inputPath ++ ".py"
+
+        raw <- readFile inputPath
+        let programMaybe = run cpFlowP raw
+
+        if isJust programMaybe then do
+            let program = snd $ fromJust programMaybe 
+            let output = snd $ dump (initialTranslatorState, program)
+            writeFile outputPath output
+            putStrLn $ "Complete. File generated at \"" ++ outputPath ++ "\""
+        else do
+            putStrLn "Syntax error(s) exists."
