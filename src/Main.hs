ae2fd1eec5ca5d7b1d4ac943662419fe5a70900e
陈湛明 <1670732206@qq.com>
2023-01-07 13:45:58 +0000

    Add option to flush translated Python to output

--- src/Main.hs
+++ src/Main.hs
@@ -2,26 +2,32 @@ module Main (main) where
 
 import CambridgePseudocodeToPython (translate)
 import System.Environment (getArgs)
+import Data.Maybe (fromJust, isNothing)
 
 main :: IO ()
 main = do
     args <- getArgs
     if null args then
-        putStrLn "No input file specified."
+        error "No input file specified."
 
     else do
-        let firstArg = head args
-        if firstArg == "--help" || firstArg == "-h" then
+        let inputPath = head $ filter (\s -> head s /= '-') args
+        if "--help" `elem` args || "-h" `elem` args then
             putStrLn "Usage: campseudo-to-py <pseudocode_source_code>"
         else do
 
-            let inputPath = firstArg
-                outputPath = inputPath ++ ".py"
+            let outputPath = inputPath ++ ".py"
 
             raw <- readFile inputPath
             let output = translate raw
 
-            maybe
-                (putStrLn "Syntax error(s) exists.")
-                (writeFile outputPath)
-                output
+            if isNothing output then
+                error "Syntax error(s) exists."
+
+            else do
+                if "-e" `elem` args then
+                    putStr $ fromJust output
+
+                else do
+                    putStrLn $ "Writing to " ++ outputPath
+                    writeFile outputPath $ fromJust output
