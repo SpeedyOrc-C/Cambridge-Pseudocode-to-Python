d4bd5f65a0c06b403c5b2472aaf2f69fd1ebc244
陈湛明 <1670732206@qq.com>
2023-01-13 12:47:26 +0000

    Create bin folder automatically when compile

--- compile.sh
+++ compile.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+if [ -d ./bin ]
+then
+    echo \"bin\" folder exists.
+else
+    echo \"bin\" folder does not exist, create it.
+    mkdir ./bin
+fi
+
 cd src
 ghc Main.hs -o ../bin/campseudo-to-py
 cd ../
